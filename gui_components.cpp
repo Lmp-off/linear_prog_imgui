0267a5f65c0f8c195dfe8991d0ac9d45a15bb6d6
phoenix381 <phoenix381@bk.ru>
2025-04-28 02:22:00 +0300

    button create func

--- gui_components.cpp
+++ gui_components.cpp
@@ -1,10 +1,14 @@
 #include "gui_components.h"
 #include "resources.h"
 
-void CreateMainControls(HWND hwnd) {
-    CreateWindow(L"BUTTON", L"Reset", WS_VISIBLE | WS_CHILD,
-        10, 10, 80, 30, hwnd, (HMENU)1, NULL, NULL);
+void createButton(HWND hwnd, int buttonId, int x, int y, int width, int height, const wchar_t* text) {
+    CreateWindow(L"BUTTON", text, WS_VISIBLE | WS_CHILD,
+        x, y, width, height, hwnd, (HMENU)buttonId, NULL, NULL);
+}
 
-    CreateWindow(L"BUTTON", L"Zoom +", WS_VISIBLE | WS_CHILD,
-        100, 10, 80, 30, hwnd, (HMENU)2, NULL, NULL);
+
+void CreateMainControls(HWND hwnd) {
+    createButton(hwnd, BUTTON_RESET, 10, 10, 80, 30, L"Reset");
+    createButton(hwnd, BUTTON_ZOOMOUT, 10, 50, 80, 30, L"Zoom -");
+    createButton(hwnd, BUTTON_ZOOMIN, 10, 90, 80, 30, L"Zoom +");
 }
--- gui_components.h
+++ gui_components.h
@@ -1,4 +1,7 @@
 #pragma once
 #include <Windows.h>
+#include "app_state.h"
+
+void createButton(HWND hwnd, int buttonId, int x, int y, int width, int height, const wchar_t* text);
 
 void CreateMainControls(HWND hwnd);
